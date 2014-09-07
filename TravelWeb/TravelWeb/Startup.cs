542b61365bcaeb7a1244a048c899f83939198816
syking <syking@gmail.com>
2014-09-07 16:28:08 +0800

    setup project

---
+++ TravelWeb/TravelWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TravelWeb.Startup))]
+namespace TravelWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
